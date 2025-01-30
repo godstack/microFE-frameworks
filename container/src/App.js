fddf377ffd842779fd31f9602ccbe92a536d96be
godstack <aleshkakrab@gmail.com>
2025-01-30 21:53:40 +0100

    fix styles collision in production

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
-import { StylesProvider } from '@material-ui/core';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
