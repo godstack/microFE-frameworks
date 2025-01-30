c4d1437a59a6fb93313ec3a5e0364266c44c63ea
godstack <aleshkakrab@gmail.com>
2025-01-30 11:39:29 +0100

    complete webpack setup for prod

--- marketing/config/webpack.dev.js
+++ marketing/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require('webpack-merge');
-const HTmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -20,9 +19,6 @@ const devConfig = {
                 './MarketingApp': './src/bootstrap'
             },
             shared: packageJson.dependencies
-        }),
-        new HTmlWebpackPlugin({
-            template: './public/index.html'
         })
     ]
 };
