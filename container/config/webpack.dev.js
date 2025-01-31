72e4651f846e839a4d4871c7d33c03bf7d571c8e
godstack <aleshkakrab@gmail.com>
2025-01-31 12:36:25 +0100

    fix routing inside the apps wle not on the just / route

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -12,9 +12,7 @@ const devConfig = {
     },
     devServer: {
         port: PORT,
-        historyApiFallback: {
-            index: 'index.html'
-        }
+        historyApiFallback: true
     },
     plugins: [
         new ModuleFederationPlugin({
