c3fe6805966c8271fe1bb2b5a18d409cfc72dc1e
godstack <aleshkakrab@gmail.com>
2025-01-31 17:53:59 +0100

    add mock auth to the project

--- container/src/App.js
+++ container/src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core';
-import AuthApp from './components/AuthApp';
+import Progress from './components/Progress';
+
+const AuthLazy = lazy(() => import('./components/AuthApp'));
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co'
 });
 
 const App = () => {
+    const [isSignedIn, setIsSignedIn] = useState(false);
+
     return (
         <StylesProvider generateClassName={generateClassName}>
             <BrowserRouter>
                 <div>
-                    <Header />
-                    <Switch>
-                        <Route path='/auth' component={AuthApp} />
-                        <Route path='/' component={MarketingApp} />
-                    </Switch>
+                    <Header
+                        isSignedIn={isSignedIn}
+                        onSignOut={() => setIsSignedIn(false)}
+                    />
+                    <Suspense fallback={<Progress />}>
+                        <Switch>
+                            <Route path='/auth'>
+                                <AuthLazy
+                                    onSignIn={() => setIsSignedIn(true)}
+                                />
+                            </Route>
+                            <Route path='/'>
+                                <MarketingLazy isSignedIn={isSignedIn} />
+                            </Route>
+                        </Switch>
+                    </Suspense>
                 </div>
             </BrowserRouter>
         </StylesProvider>
